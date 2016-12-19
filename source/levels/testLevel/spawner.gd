2e156a324676cdfde7fcd354abc3da08e85035bc
Henrique Campos <henriiquecampos@gmail.com>
2016-12-19 20:07:01 -0200

    Added Spawner
    I don't know if I'll use a moving character or a moving scene aproach to
    this game. So I'll consult the guys on discord for that

--- source/levels/testLevel/spawner.gd
+++ source/levels/testLevel/spawner.gd
@@ -2,4 +2,15 @@ extends Position2D
 
 export (PackedScene) var objectScene
 onready var objectPath = objectScene.get_path()
-var object = load(objectPath)
+onready var object = load(objectPath)
+onready var mainNode = get_parent()
+
+onready var timer = get_node("timer")
+func _ready():
+	timer.start()
+
+func _on_timer_timeout():
+	randomize()
+	var newObjectInstance = object.instance()
+	mainNode.add_child(newObjectInstance)
+	newObjectInstance.set_pos(get_pos())
