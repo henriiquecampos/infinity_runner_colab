2d67176ae8411d034d2ce64089d47f2bd250a164
Henrique Campos <henriiquecampos@gmail.com>
2016-12-19 22:55:25 -0200

    Major spawns working
    Even tho they are working I don't feel like the spawned objects will
    move...I'm more in the mood of making the player character move

--- source/levels/testLevel/spawner.gd
+++ source/levels/testLevel/spawner.gd
@@ -5,12 +5,20 @@ onready var objectPath = objectScene.get_path()
 onready var object = load(objectPath)
 onready var mainNode = get_parent()
 
+var lastObject
 onready var timer = get_node("timer")
 func _ready():
+	randomize()
+	timer.set_wait_time(rand_range(0.2,2.0))
 	timer.start()
 
 func _on_timer_timeout():
-	randomize()
 	var newObjectInstance = object.instance()
 	mainNode.add_child(newObjectInstance)
 	newObjectInstance.set_pos(get_pos())
+	lastObject = newObjectInstance
+	lastObject.get_node("visibilityNotifier").connect("enter_screen", self, "resetSpawn")
+	timer.set_wait_time(rand_range(0.2,2.0))
+	
+func resetSpawn():
+	timer.start()
